0444b63f67ab9d816c35fd760250a0367ba64d93
Youssef Bahaa <youssefbahaa211@gmail.com>
2025-06-15 14:05:36 +0300

    j

--- server/src/routes/reviews.ts
+++ server/src/routes/reviews.ts
@@ -6,14 +6,15 @@ import {
     updateReview,
     deleteReview,
 } from '../controllers/review.controller';
+import { authenticate } from '../middlewares/auth.middleware';
 import { isAdmin } from '../middlewares/admin.middleware';
 
 const router = Router();
 
 router.get('/', getReviews);
 router.get('/:id', getReview);
-router.post('/', createReview);
-router.put('/:id', isAdmin, updateReview);
-router.delete('/:id', isAdmin, deleteReview);
+router.post('/', authenticate, createReview);
+router.put('/:id', authenticate, isAdmin, updateReview);
+router.delete('/:id', authenticate, isAdmin, deleteReview);
 
 export default router;
